ae1e277b2f936c99df048af169de64d91dd1da93
cozekouzi <omarkouzi81248@gmail.com>
2023-01-22 16:49:53 +0000

    fixing bugs

---
+++ models/admin.js
@@ -0,0 +1,48 @@
+
+import mongoose from 'mongoose';
+const {Schema, model} = mongoose;
+
+const adminSchema = new Schema(
+{
+    name: {
+    type: String,
+    required: true
+},
+    email: {
+        type: String,
+        required: true
+},
+    password: {
+    type: String,
+    required: true
+},
+    title: {
+        type: String,
+        required: true
+},
+    LinkedinUrl: {
+    type: String,
+    
+},
+    InstagramUrl: {
+        type: String,
+        
+},
+    GithubUrl: {
+    type: String,
+    
+}
+
+}, 
+
+{
+collection: 'admin',
+timestamps: true,
+created_at: { type: Date },
+updated_at: { type: Date }
+}
+
+);
+
+const Admin = model('Admin', adminSchema);
+export default Admin;
---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+
+import express from 'express';
+const router = express.Router();
+// import controller from ' ';
+
+//routes go here
+
+export default router;
+
