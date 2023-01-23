571e1ca2f8f34634769b745b5190bbb11b29fc13
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-01-24 01:30:42 +0200

    finished CRUD info

---
+++ controllers/admin.js
@@ -0,0 +1,19 @@
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URL, {
+            useUnifiedTopology: true,
+            dbName: process.env.DB_NAME
+        })
+
+        console.log(`Connected Successfuly to Database :)`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`)
+        process.exit();
+    }
+}
+
+export default connectDB;
