0370a5129befba8115ac9649efa9d4506c594716
linarawwas <linamkraw2@gmail.com>
2023-01-24 16:12:28 +0100

    merged with lina

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,52 +7,13 @@ createAdmin = async(req, res) => {
         res.status(400).json({message:error})
     }
     else{
-        console.log("hello");
-        // const post_admin= await admin.create({
-        //         name: req.body.name,
-        //         email: req.body.email,
-        //         password: req.body.password,
-        //         title:req.body.title,
-        //         LinkedinUrl: req.body.LinkedinUrl,
-        //         InstagramUrl: req.body.InstagramUrl,
-        //         GithubUrl:req.body.GithubUrl
-        // })
+        console.log("admin was created");
         const post_admin= await admin.create(req.body)
         console.log(post_admin)
         return res.status(200).json(post_admin);
     }
     
 }
-    /*    try {
-        let body = req.body;
-        console.log("body ",req.body)
-        let new_Admin = new Admin(body);
-        const admin = await new_Admin.save();
-        return res.status(200).json({ success: true })
-
-    } catch(error){
-        return res.json({ error: error.message })
-    }
-}
-
-// async createAdmin(req, res) {
-//     const body = req.body;
-//         try {
-            
-//         const doc = new Admin(body);
-//         const new_admin = await doc.save()
-//         return res.status(200).json({ success: true , new_admin});
-//         }
-
-//     catch(error){
-//         return res.status(500).json({ 
-//             error: error.message,
-//             success:false,
-//             data: error.message
-//     })
-//     }
-// }*/
-
 
 
 getAdminById = async(req, res) => {
