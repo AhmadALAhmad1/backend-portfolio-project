34fe16e9d6f86facacf6c132479a3b0d384ac0c0
cozekouzi <omarkouzi81248@gmail.com>
2023-01-25 11:25:02 +0000

    cleaned from commented txt

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,28 +2,6 @@ import Admin from "../models/admin.js";
 
 
 class Controller{
-//for create 
-// async createAdmin(req, res) {
-
-  //   const body = req.body;
-
-  //   try {
-
-  //     const doc = new Admin(body);
-  //     const new_date = await doc.save()
-
-  //     return res.status(200).json({ success: true, new_date });
-  //   }
-
-  //   catch (err) {
-  //     return res.status(500).json({
-  //       status: 500,
-  //       success: false,
-  //       data: err.message
-  //     })
-  //   }
-  // }
-
 // for update 
 updateAdmin = async(req, res) => {
     
@@ -40,7 +18,7 @@ updateAdmin = async(req, res) => {
     }
 }
 
-
+// for delete
 async deleteAdmin (req, res, next) {
     let { id } = req.params;
     const find = await Admin.findById(id);
@@ -59,29 +37,6 @@ async deleteAdmin (req, res, next) {
       message: "deleted successfully"
     })
   }
-
-// for delete: 
-// deleteAdmin= async(req, res) => {
-    
-//     const n = req.params.id
-//     try {
-        
-//         if (n <= Admin.length)
-//     {
-//         Admin.splice(n - 1, 1 )
-//         res.json({ status: 200 ,message:`deleted successfuly`, message: Admin})
-    
-//     }
-//     else 
-//     {
-//     return res.status(404).json({status:404, error:true, message:`The admin with id = ${n} does not exist`})
-//     }
-    
-//     }
-//      catch(error){
-//         return res.json({ error: error.message })
-//     }
-// }
 }
 
 const controller = new Controller()
--- routes/info.js
+++ routes/info.js
@@ -1,8 +1,5 @@
 
 import express from 'express';
 const router = express.Router();
-// import controller from ' ';
-
-//routes go here
 
 export default router;
