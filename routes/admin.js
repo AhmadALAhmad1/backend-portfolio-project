d03006c55bcb180dbcc3be549987241b20795c7b
cozekouzi <omarkouzi81248@gmail.com>
2023-01-25 11:21:35 +0000

    deleted commented text on routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import controller from '../controllers/admin.js'
-
-// import listAdmins from '.././controllers/admin.js';
-// import getAdminById from '.././controllers/admin.js';
-// import createAdmin from '.././controllers/admin.js';
-
+import controller from '../controllers/admin.js';
 
 router.get('/', controller.listAdmins) //get all admins
 router.get('/:id', controller.getAdminById) // get specific admin
